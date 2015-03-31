0b6de31cfcd7f423f8c1a00e27ccb05765b1fbc6
Shawn Kumar <shawn.kumar@datastax.com>
2015-03-31 08:46:10 -0700

    add exclusive connection

--- cluster.py
+++ cluster.py
@@ -18,9 +18,21 @@ class ValCluster(object):
     def get_nodes(self):
         return self.nodelist
 
-    def get_session(self):
-        cluster = Cluster(self.hosts)
-        session = cluster.connect()
+    def get_session(self, nodes, keyspace=None, exclusive=False):
+        node_ips = []
+        for node in nodes:
+            node_ips.append(node.get_address())
+
+        if exclusive = True:
+            wlrr = WhiteListRoundRobinPolicy([node_ip])
+            cluster = Cluster(node_ips, load_balancing_policy=wlrr)
+            session = cluster.connect()
+        else:
+            cluster = Cluster(node_ips)
+            session = cluster.connect()
+        
+        if keyspace is not None:
+            session.execute('USE %s' % keyspace)
         return session
 
     def stress(self, command, nodes, parallel=False):
@@ -34,6 +46,6 @@ class ValCluster(object):
         hosts = "-node " + hosts
         line = "{base} {cmd} {hosts}".format(base=base, cmd=command, hosts=hosts)
         if parallel == True:
-            p = Popen(line)
+            p = Popen(line, shell=True)
         else:
             local(line)
